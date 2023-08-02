200ce87b714cfafdeeba2c958d1bfbaa40dbdb4c
suri312006@gmail.com <suri312006@gmail.com>
2023-08-01 17:34:33 -0700

    commit before using generic views

--- Django_Tutorial/mysite/polls/models.py
+++ Django_Tutorial/mysite/polls/models.py
@@ -1,9 +1,11 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 class Question(models.Model):
-    #fields will be stored in the database
+    # fields will be stored in the database
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -13,9 +15,10 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+
 class Choice(models.Model):
-    #idk what foreign key means
-    #foreign key = each Choice is related to a single Question.
+    # idk what foreign key means
+    # foreign key = each Choice is related to a single Question.
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
