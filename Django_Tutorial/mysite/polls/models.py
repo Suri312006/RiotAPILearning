c9645d72c6921d8e3c8792011ea0d6558a7919a6
suri312006 <suri312006@gmail.com>
2023-08-03 16:48:32 -0700

    part 7 progress

--- Django_Tutorial/mysite/polls/models.py
+++ Django_Tutorial/mysite/polls/models.py
@@ -1,17 +1,25 @@
 import datetime
 from django.db import models
 from django.utils import timezone
-
+from django.contrib import admin
 
 # Create your models here.
 class Question(models.Model):
     # fields will be stored in the database
+
+
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return "Question: {}, Date Published: {}".format(self.question_text, self.pub_date)
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description = "Published recently?"
+    )
     def was_published_recently(self):
         time_diff = 1
 
